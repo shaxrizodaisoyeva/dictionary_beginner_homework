d1bc2a633bd6f4eed41e2e5577f8a4e7d9ddafb3
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-18 10:51:08 +0500

    done

--- find_max_key.py
+++ find_max_key.py
@@ -6,4 +6,12 @@ def find_max_key(data: dict):
     Returns:
         int: The maximum key in the dictionary.
     """
-    return 
+    a=0 
+    b=[]
+    while a<len(data):
+        b+=data.keys()
+        a +=1
+    c=max(b)
+    return c
+data={3.4: 'abc', 5:'deval', 9.8: 'bbasb', 9.78:'ttea', 23:'asccd', 7.9:'thy'}
+print(find_max_key(data))  
