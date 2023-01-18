b9d8cbe2d96dc1c1df0502eee80974627acb9107
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-18 14:44:00 +0500

    Done

--- sum_dict_values.py
+++ sum_dict_values.py
@@ -5,5 +5,14 @@ def sum_dict_values(data: dict) -> int:
         data (dict): A dictionary of values
     Returns:
         int: The sum of all values in the dictionary
-    '''
-    return 
+    ''' 
+    sum=0
+    b=list(data.values())
+    for i in b:
+        sum+=i
+    return sum
+data={'name':2,
+'age':34, 'nae':43,
+'ge':34, 'nme':23,
+'ae':35}
+print(sum_dict_values(data))  
