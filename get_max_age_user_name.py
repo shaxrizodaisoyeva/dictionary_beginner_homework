e951d83cbb1098f410d3091f7a3e427fc796a3de
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-18 14:05:29 +0500

    Done

--- get_max_age_user_name.py
+++ get_max_age_user_name.py
@@ -15,7 +15,7 @@ def get_max_age_user_name(data:list) -> str:
         a+=1
     c+=b[1::2]
     d=c.index(max(c))
-    e=b[2*d]
+    e= b[2*d]
     return e
 data=[{'name':"a",
 'age':34}, {'name':"b",
