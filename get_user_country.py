c5151fdfaab15bb7aa3248337dd7da9c094413e4
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-18 14:58:08 +0500

    Done

--- get_user_country.py
+++ get_user_country.py
@@ -13,8 +13,11 @@ def get_user_country(data:list, name:str) -> list:
     while a<len(data):
         b+=data[a].values()
         a+=1
-    c=b.index(name)
-    d =b[c+1]
+    if name not in b:
+        d="Not found"
+    else:
+        c=b.index(name)
+        d =b[c+1]
     return d
 data=[{'name': 'A', 'country':'USA'}, {'name': 'B', 'country':'UK'}, {'name':'C', 'country':'Uzb'}]
 name='d'
